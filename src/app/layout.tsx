151da07ba10f6be2ae56bc327e2482c4afb8537a
Manish Singh <smanish@Manishs-MacBook-Air.local>
2026-05-18 15:39:14 +0530

    Fix Inter font import in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { Header } from "@/components/layout/Header";
