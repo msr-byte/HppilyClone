3e7a7a3ba42c33155e839ea76ad35d4afcb68a16
Manish Singh <smanish@Manishs-MacBook-Air.local>
2026-05-18 16:06:19 +0530

    Embed Sanity Studio into Next.js

---
+++ sanity.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { schemaTypes } from './sanity/schemas'
+
+export default defineConfig({
+  basePath: '/studio',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
+  title: 'SaaSReplica CMS',
+  schema: {
+    types: schemaTypes,
+  },
+  plugins: [
+    structureTool(),
+  ],
+})
